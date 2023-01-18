87480dd865da62fad6cd214acfcbc5163b3fca01
NieamulKabir <kabir000079@gmail.com>
2023-01-19 00:20:08 +0600

    update

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import apiSlice from '../features/api/apiSlice';
 import authSlice from '../features/auth/authSlice';
 
 const store = configureStore({
     reducer: {
+        [apiSlice.reducer]: apiSlice.reducer,
         auth: authSlice,
     },
 })
